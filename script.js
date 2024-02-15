1583509b698b321deff548589d8bc28fef92f8d3
sofus64 <157691346+sofus64@users.noreply.github.com>
2024-02-15 10:25:52 +0100

    Revert "Update script.js"
    This reverts commit 9dbaa8047e9323852d6269aae69da1c062f0729e.

--- script.js
+++ script.js
@@ -19,10 +19,8 @@ let backgroundMusic = true;
 let rukeListe = [];
 let rukeListeCap = 10;                             /*!!!!!!!!*/
 let rukeTimer;
-let happinessDifficulty = 10;
-let hungerDifficulty = 10;
 
-
+//peakaboo
 //Frode's mesterverk
 const dead = new Audio('lyder/drept.mp3')
 const fed = new Audio('lyder/godtmedmat.mp3')
@@ -43,9 +41,6 @@ function startScreen() {
     <div class="nameText">Name: </div>                                                    <!--!!!!!!!!!!!-->
     <input type="text" onchange="updateName(this.value)" placeholder="Frode.."/>        <!--!!!!!!!!!!!-->
         <button onclick=startGame()>Start Game</button>
-        <button onclick=chooseDifficulty('5', '5')>Easy Mode</button>
-        <button onclick=chooseDifficulty('10', '10')>Normal Mode</button>
-        <button onclick=chooseDifficulty('15', '15')>Hard Mode</button>
     </div>
     `;
 }
@@ -158,8 +153,8 @@ function playButtonCooldown() {
 }
 
 function hungryAndSad() {
-    goatHappiness -= happinessDifficulty;
-    goatHunger -= happinessDifficulty;
+    goatHappiness -= 10;
+    goatHunger -= 10;
     updateView();
 }
 
@@ -261,9 +256,4 @@ function fjernRuke(ruke) {
     }
     lageRuker();
     updateView();
-}
-
-function chooseDifficulty(happiness, hunger){
-    happinessDifficulty = happiness;
-    hungerDifficulty = hunger;
 }
